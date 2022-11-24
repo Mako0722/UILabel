f32163a77f06372726fc0399be2c9ae055fe39bc
Mako0722 <zhongtiancheng79@gmail.com>
2022-11-24 21:12:00 +0900

    first commit

---
+++ UILabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by 中田誠 on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
